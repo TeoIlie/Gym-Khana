8ecf53cde71d55fca3f47e4b868fcf918c7b91f5
Teodor Ilie <teo.altum.quinque@gmail.com>
2025-11-24 14:40:30 -0500

    create parallel drift race train file

--- train/ppo_race.py
+++ train/ppo_race.py
@@ -3,39 +3,45 @@ import os
 import gymnasium as gym
 import numpy as np
 from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from wandb.integration.sb3 import WandbCallback
-from train.config.env_config import PROJECT_NAME, SEED
-from train.training_utils import get_output_dirs, make_output_dirs, get_ckpt_callback
+from train.config.env_config import N_ENVS, PROJECT_NAME, SEED
+from train.training_utils import get_output_dirs, make_env, make_output_dirs, get_ckpt_callback
 
 # toggle this to train or evaluate
-train = True
+TRAIN = True
+
+# config for train and test
+CONFIG = {
+    "map": "Spielberg",
+    "num_agents": 1,
+    "timestep": 0.01,
+    "num_beams": 36,
+    "integrator": "rk4",
+    "control_input": ["speed", "steering_angle"],
+    "observation_config": {"type": "rl"},
+    "reset_config": {"type": "rl_random_static"},
+    "normalize_act": False,
+    "normalize_obs": False,
+    "predictive_collision": True,
+    "wall_deflection": True,
+}
 
 
 def main():
-    if train:
+    if TRAIN:
         proj_root, output_root = get_output_dirs()
 
         run = wandb.init(project=PROJECT_NAME, sync_tensorboard=True, monitor_gym=True, dir=proj_root, save_code=True)
         run_id = run.id
+
+        print("=" * 40)
+        print("PPO Race Training")
+        print("=" * 40)
+
         tensorboard_dir, models_dir, videos_dir = make_output_dirs(run.id, output_root)
 
-        env = gym.make(
-            "f1tenth_gym:f1tenth-v0",
-            config={
-                "map": "Spielberg",
-                "num_agents": 1,
-                "timestep": 0.01,
-                "num_beams": 36,
-                "integrator": "rk4",
-                "control_input": ["speed", "steering_angle"],
-                "observation_config": {"type": "rl"},
-                "reset_config": {"type": "rl_random_static"},
-                "normalize_act": True,
-                "normalize_obs": False,
-                "predictive_collision": True,
-                "wall_deflection": True,
-            },
-        )
+        env = SubprocVecEnv([make_env(seed=SEED, rank=i, config=CONFIG) for i in range(N_ENVS)])
 
         model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=tensorboard_dir, device="auto", seed=SEED)
         model.learn(
@@ -50,17 +56,8 @@ def main():
         final_model_path = f"{models_dir}/ppo_drift_final_{run_id}"
         model.save(final_model_path)
 
-        # If debugging observation normalization, explicitly close the environment to
-        # trigger observation statistics printing -> env is wrapped in DummyVecEnv, so we need to close the underlying env
-        # if debug_obs_norm:
-        #     if hasattr(env, "envs"):
-        #         # VecEnv wrapper - close the underlying environments
-        #         for underlying_env in env.envs:
-        #             underlying_env.close()
-        #     else:
-        #         # Regular env
-        #         env.close()
         env.close()
+
         run.finish()
 
     else:
@@ -70,20 +67,7 @@ def main():
         model = PPO.load(model_path, print_system_info=True, device="cpu")
         eval_env = gym.make(
             "f1tenth_gym:f1tenth-v0",
-            config={
-                "map": "Spielberg",
-                "num_agents": 1,
-                "timestep": 0.01,
-                "num_beams": 36,
-                "integrator": "rk4",
-                "control_input": ["speed", "steering_angle"],
-                "observation_config": {"type": "rl"},
-                "reset_config": {"type": "rl_random_static"},
-                "normalize_act": False,
-                "normalize_obs": False,
-                "predictive_collision": True,
-                "wall_deflection": True,
-            },
+            config=CONFIG,
             render_mode="human",
         )
         np.random.seed()
