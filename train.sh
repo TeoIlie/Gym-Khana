3189aa88d9cec85152530a82a47d812e42a528c1
TeoIlie <teo.altum.quinque@gmail.com>
2026-02-26 13:44:58 -0500

    added train script to repo for git tracking

---
+++ train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=f1tenth_ppo
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=128G
+#SBATCH --time=24:00:00
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=teodor.ilie@queensu.ca
+
+module load python/3.11 scipy-stack gcc opencv
+source ~/envs/f1tenth/bin/activate
+cd ~/F1TENTH_Gym
+python3 train/ppo_recover.py --m t
