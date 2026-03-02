ccb6085d359cf17cc9801d15d1d99ad79725c1d8
Teodor Ilie <teo.altum.quinque@gmail.com>
2026-03-02 12:30:54 -0500

    more frequent cktp logging, new train script for naive knowledge transfer

---
+++ retrain_race_naive.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=f1tenth_ppo
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=128G
+#SBATCH --time=21:00:00
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=teodor.ilie@queensu.ca
+
+module load python/3.11 scipy-stack gcc opencv
+source ~/envs/f1tenth/bin/activate
+cd ~/F1TENTH_Gym
+python3 train/ppo_recover.py --m c --path outputs/downloads/178a1a5l/model.zip --additional_timesteps 100000000
